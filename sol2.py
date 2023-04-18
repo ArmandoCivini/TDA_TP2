ebdc106efaab24f126809ddb4dd8323295bc0ff0
Tomas Civini <tcivini@gmail.com>
2023-04-18 12:10:57 -0300

    legibility changes

--- sol2.py
+++ sol2.py
@@ -15,6 +15,12 @@ def cargar_puntos(archivo):
 def calcular_peso(p1, p2, p3):
     return distancia(p1, p2) + distancia(p2, p3) + distancia(p3, p1)
 
+def peso_poligono(triangulaciones, poligono, j, k, poligon_len):
+    peso_triangulo = calcular_peso(poligono[j], poligono[k], poligono[j+poligon_len])
+    peso_prev = triangulaciones[j][k]
+    peso_post = triangulaciones[k][j+poligon_len]
+    return peso_triangulo + peso_prev + peso_post
+
 def triangular(poligono):
     n = len(poligono)
     triangulaciones = [[None] * n for _ in range(n)]
@@ -24,9 +30,10 @@ def triangular(poligono):
         if i < n-1:
             triangulaciones[i][i+1] = 0
     
-    for i in range(2, n):
-        for j in range(n-i):
-            triangulaciones[j][(j+i)] = min([calcular_peso(poligono[j], poligono[k], poligono[j+i]) + triangulaciones[j][k] + triangulaciones[k][j+i] for k in range(j+1,j+i)])
+    for poligon_len in range(2, n):
+        for j in range(n-poligon_len):
+            triangulaciones[j][(j+poligon_len)] = \
+                min([peso_poligono(triangulaciones, poligono, j, k, poligon_len) for k in range(j+1,j+poligon_len)])
 
     print(triangulaciones)
     return triangulaciones[0][n-1]
