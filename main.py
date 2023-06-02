a4161bed0e8bc55f227f997467437d70dce108ea
Tomas Civini <tcivini@gmail.com>
2023-06-02 15:37:26 -0300

    linted files

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import sys
 
 
 def distancia(p, q):
-    return ((p[0] - q[0])**2 + (p[1] - q[1])**2) ** 0.5
+    return ((p[0] - q[0]) ** 2 + (p[1] - q[1]) ** 2) ** 0.5
 
 
 def sumar_lados(triangulo):
-    return distancia(triangulo[0], triangulo[1]) + distancia(triangulo[0], triangulo[2]) + distancia(triangulo[1], triangulo[2])
-
-
-def crear_subpoligono(poligono, vertices_excluidos):
-    return [v for v in poligono if v not in [poligono[u] for u in vertices_excluidos]]
+    return (
+        distancia(triangulo[0], triangulo[1])
+        + distancia(triangulo[0], triangulo[2])
+        + distancia(triangulo[1], triangulo[2])
+    )
 
 
 def calcular_peso(triangulo, poligono):
@@ -31,7 +31,10 @@ def es_triangulo_valido(triangulo, indices_subpoligono):
 def mapear_indices_a_vertices(poligono, triangulacion):
     triangulos = []
     for triangulo in triangulacion:
-        triangulos.append((poligono[triangulo[0]], poligono[triangulo[1]], poligono[triangulo[2]]))
+        triangulos.append(
+            (poligono[triangulo[0]], poligono[triangulo[1]],
+                poligono[triangulo[2]])
+        )
 
     return triangulos
 
@@ -40,9 +43,14 @@ def cargar_puntos(archivo):
     puntos = []
     with open(archivo) as f:
         for linea in f:
-            coordenadas = linea.rstrip('\n').split(" ")
-            puntos.append((float(coordenadas[0].replace(",",".")), float(coordenadas[1].replace(",","."))))
-    
+            coordenadas = linea.rstrip("\n").split(" ")
+            puntos.append(
+                (
+                    float(coordenadas[0].replace(",", ".")),
+                    float(coordenadas[1].replace(",", ".")),
+                )
+            )
+
     return puntos
 
 
@@ -50,46 +58,64 @@ def imprimir_resultado(triangulacion, peso):
     print("Triángulos a armar: \n")
     for i in range(len(triangulacion)):
         print(f"Triángulo {i+1}: {triangulacion[i]}")
-    print(f"Sumatoria total de la triangulacion: {peso}") 
+    print(f"Sumatoria total de la triangulacion: {peso}")
 
 
 def crear_subpoligono(poligono, indices_subpoligono):
     return [poligono[i] for i in indices_subpoligono]
 
 
-
-def triangular(poligono): 
+def triangular(poligono):
     triangulaciones = {}
     triangulaciones[0] = {}
     n = len(poligono)
     indice = {}
-    
+
     for i in range(n):
         indice[poligono[i]] = i
-        triangulo = crear_triangulo(i, (i+1)%n, (i+2)%n)   #cada triangulo esta formado por los indices de los vertices en poligono
-        triangulaciones[0][(triangulo, )] = {"peso": calcular_peso(triangulo, poligono), "indices_subpoligono": {k for k in range(n) if k != (i+1)%n}}
-    
-    for i in range(1, n-2):
+        triangulo = crear_triangulo(
+            i, (i + 1) % n, (i + 2) % n
+        )  # cada triangulo esta formado por los indices
+        # de los vertices en poligono
+
+        triangulaciones[0][(triangulo,)] = {
+            "peso": calcular_peso(triangulo, poligono),
+            "indices_subpoligono": {k for k in range(n) if k != (i + 1) % n},
+        }
+
+    for i in range(1, n - 2):
         triangulaciones[i] = {}
-            
-        for triangulos, info in triangulaciones[i-1].items():
+
+        for triangulos, info in triangulaciones[i - 1].items():
             indices_subpoligono = info["indices_subpoligono"]
             peso = info["peso"]
             subpoligono = crear_subpoligono(poligono, indices_subpoligono)
             m = len(subpoligono)
-            
-            for j in range(m):
-                nuevo_triangulo = crear_triangulo(indice[subpoligono[j]], indice[subpoligono[(j+1)%m]], indice[subpoligono[(j+2)%m]])
-                if nuevo_triangulo not in triangulos:
-                    triangulaciones[i][tuple(sorted(list(triangulos) + [nuevo_triangulo]))] = {"peso" : peso + calcular_peso(nuevo_triangulo, poligono), "indices_subpoligono" : indices_subpoligono - {indice[subpoligono[(j+1)%m]]}}
 
-    triangulacion_minima = min(triangulaciones[n-3].keys(), key=lambda triangulacion: triangulaciones[n-3][triangulacion]["peso"])
-    peso_minimo = triangulaciones[n-3][triangulacion_minima]["peso"]
+            for j in range(m):
+                nuevo_triangulo = crear_triangulo(
+                    indice[subpoligono[j]],
+                    indice[subpoligono[(j + 1) % m]],
+                    indice[subpoligono[(j + 2) % m]],
+                )
+                if nuevo_triangulo not in triangulos:
+                    triangulaciones[i][
+                        tuple(sorted(list(triangulos) + [nuevo_triangulo]))
+                    ] = {
+                        "peso":
+                            peso + calcular_peso(nuevo_triangulo, poligono),
+                        "indices_subpoligono": indices_subpoligono
+                        - {indice[subpoligono[(j + 1) % m]]},
+                    }
+
+    triangulacion_minima = min(
+        triangulaciones[n - 3].keys(),
+        key=lambda triangulacion:
+        triangulaciones[n - 3][triangulacion]["peso"],
+    )
+    peso_minimo = triangulaciones[n - 3][triangulacion_minima]["peso"]
 
     return triangulacion_minima, peso_minimo
-
-
-
 
 
 def main():
--- sol2.py
+++ sol2.py
@@ -1,55 +1,91 @@
 import sys
 
+
 def distancia(p, q):
-    return ((p[0] - q[0])**2 + (p[1] - q[1])**2) ** 0.5
+    return ((p[0] - q[0]) ** 2 + (p[1] - q[1]) ** 2) ** 0.5
+
 
 def cargar_puntos(archivo):
     puntos = []
     with open(archivo) as f:
         for linea in f:
-            coordenadas = linea.rstrip('\n').split(" ")
-            puntos.append((float(coordenadas[0].replace(",",".")), float(coordenadas[1].replace(",","."))))
-    
+            coordenadas = linea.rstrip("\n").split(" ")
+            puntos.append(
+                (
+                    float(coordenadas[0].replace(",", ".")),
+                    float(coordenadas[1].replace(",", ".")),
+                )
+            )
+
     return puntos
+
 
 def calcular_peso(p1, p2, p3):
     return distancia(p1, p2) + distancia(p2, p3) + distancia(p3, p1)
 
-def peso_poligono(triangulaciones, poligono, poligono_inicio, sub_triang, poligon_len):
-    #el peso del poligono es el peso del triangulo mas el peso de los poligonos que quedan
-    #entre el punto y los extremos
-    peso_triangulo = calcular_peso(poligono[poligono_inicio], poligono[sub_triang], poligono[poligono_inicio+poligon_len])
+
+def peso_poligono(
+        triangulaciones, poligono, poligono_inicio, sub_triang, poligon_len):
+    # el peso del poligono es el peso del triangulo
+    # mas el peso de los poligonos que quedan
+    # entre el punto y los extremos
+    peso_triangulo = calcular_peso(
+        poligono[poligono_inicio],
+        poligono[sub_triang],
+        poligono[poligono_inicio + poligon_len],
+    )
     peso_prev = triangulaciones[poligono_inicio][sub_triang]
-    peso_post = triangulaciones[sub_triang][poligono_inicio+poligon_len]
+    peso_post = triangulaciones[sub_triang][poligono_inicio + poligon_len]
     return peso_triangulo + peso_prev + peso_post
 
-def triangular(poligono):
-    #TODO: memorizar triangulos elegidos
-    n = len(poligono)
-    triangulaciones = [[None] * n for _ in range(n)] #genero matriz dinamica
-    
-    for i in range(n): #seteo los valores que no pueden ser triangulos a 0
-        triangulaciones[i][i] = 0
-        if i < n-1:
-            triangulaciones[i][i+1] = 0
-    
-    for poligon_len in range(2, n): 
-        #itero empezando por los poligonos de menor tamaño a los de mayor tamaño
-        for poligono_inicio in range(n-poligon_len):
-            #por cada punto del poligono genero el poligono de tamaño poligon_len trazando
-            #una linea entre el punto y el punto y poligon_len posiciones mas adelante
-            triangulaciones[poligono_inicio][(poligono_inicio+poligon_len)] = \
-                min([peso_poligono(triangulaciones, poligono, poligono_inicio, sub_triang, poligon_len) \
-                for sub_triang in range(poligono_inicio+1,poligono_inicio+poligon_len)])
-            #dentro de ese poligono itero por cada uno de sus puntos que no sean extremos y
-            #formo un triangulo entre ese punto y los extremos(poligono_inicio+1,poligono_inicio+poligon_len)
-            
 
-    return triangulaciones[0][n-1] #este valor representa el peso del poligono completo
+def triangular(poligono):
+    # TODO: memorizar triangulos elegidos
+    n = len(poligono)
+    triangulaciones = [[None] * n for _ in range(n)]  # genero matriz dinamica
+
+    for i in range(n):  # seteo los valores que no pueden ser triangulos a 0
+        triangulaciones[i][i] = 0
+        if i < n - 1:
+            triangulaciones[i][i + 1] = 0
+
+    for poligon_len in range(2, n):
+        # itero empezando por los poligonos de menor tamaño
+        # a los de mayor tamaño
+        for poligono_inicio in range(n - poligon_len):
+            # por cada punto del poligono genero el poligono
+            # de tamaño poligon_len trazando
+            # una linea entre el punto y el punto
+            # y poligon_len posiciones mas adelante
+            triangulaciones[poligono_inicio][(poligono_inicio + poligon_len)] \
+                = min(
+                    [
+                        peso_poligono(
+                            triangulaciones,
+                            poligono,
+                            poligono_inicio,
+                            sub_triang,
+                            poligon_len,
+                        )
+                        for sub_triang in range(
+                            poligono_inicio + 1, poligono_inicio + poligon_len
+                        )
+                    ]
+                )
+            # dentro de ese poligono itero
+            # por cada uno de sus puntos que no sean extremos y
+            # formo un triangulo entre ese punto y
+            # los extremos(poligono_inicio+1,poligono_inicio+poligon_len)
+
+    return triangulaciones[0][
+        n - 1
+    ]  # este valor representa el peso del poligono completo
+
 
 def main():
     poligono = cargar_puntos(sys.argv[1])
     peso = triangular(poligono)
     print(peso)
+
 
 main()
